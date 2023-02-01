67d7fdff28357420c1412d8ec9cbbabc9b3e9ccc
DAOVANLOI2K1 <loinhung4@gmail.com>
2023-02-01 16:44:27 +0700

    anh loi up

---
+++ Nhom2_usol_Api_QLKHO/Nhom2_usol_Api_QLKHO/Controllers/HangController.cs
@@ -0,0 +1,182 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Nhom2_usol_Api_QLKHO.Entities;
+using Swashbuckle.AspNetCore.Annotations;
+using System;
+
+namespace Nhom2_usol_Api_QLKHO.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HangController : ControllerBase
+    {
+        /// <summary>
+        /// Api lấy thông tin tất cả mặt hàng
+        /// </summary>
+        /// <returns>Danh sách mặt hàng</returns>
+        [HttpGet]
+        [SwaggerResponse(StatusCodes.Status200OK, type: typeof(Hang))]
+        [SwaggerResponse(StatusCodes.Status400BadRequest)]
+        [SwaggerResponse(StatusCodes.Status500InternalServerError)]
+        public IActionResult GetAllHang()
+        {
+            try
+            {
+                // kết nối db
+                var connectionString = "Server=LOIDV-IT-PC1\\SQLEXPRESS;Database=QLKHO;Trusted_Connection=true";
+                SqlConnection connection = new SqlConnection(connectionString);
+                connection.Open();
+
+                // Chuẩn bị query 
+                String getQuery = "SELECT * FROM HANG;";
+
+                //Thực thi query
+                var result = connection.Query<Hang>(getQuery);
+                connection.Close();
+                //Trả về
+                if (result == null)
+                {
+                    return StatusCode(StatusCodes.Status400BadRequest);
+                }
+                return StatusCode(StatusCodes.Status200OK, result);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest);
+            }
+        }
+
+        /// <summary>
+        /// Api xóa hàng theo mã Hàng
+        /// </summary>
+        /// <param name="mahang"></param>
+        /// <returns>Mã hàng của hàng bị xóa</returns>
+        [HttpDelete("{mahang}")]
+        [SwaggerResponse(StatusCodes.Status200OK, type: typeof(string))]
+        [SwaggerResponse(StatusCodes.Status400BadRequest)]
+        [SwaggerResponse(StatusCodes.Status500InternalServerError)]
+        public ActionResult deleteHangByMaHang([FromRoute] string? mahang)
+        {
+            try
+            {
+                // kết nối db
+                var connectionString = "Server=LOIDV-IT-PC1\\SQLEXPRESS;Database=QLKHO;Trusted_Connection=true";
+                SqlConnection connection = new SqlConnection(connectionString);
+                connection.Open();
+
+                // Chuẩn bị query 
+                String deleteQuery = "DELETE FROM HANG WHERE MaHang = @mahang;";
+
+                // Chuẩn bị param
+                DynamicParameters parameters = new DynamicParameters();
+                parameters.Add("@mahang", mahang);
+
+                //Thực thi query
+                var result = connection.Execute(deleteQuery, parameters);
+                connection.Close();
+                //Trả về
+                if (result > 0)
+                {
+                    return StatusCode(StatusCodes.Status200OK, mahang);
+                }
+                return StatusCode(StatusCodes.Status400BadRequest);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest);
+            }
+        }
+
+        /// <summary>
+        /// Api tạo mặt hàng mới
+        /// </summary>
+        /// <param name="hang"></param>
+        /// <returns>mã hàng đc tạo</returns>
+        [HttpPost]
+        [SwaggerResponse(StatusCodes.Status200OK, type: typeof(string))]
+        [SwaggerResponse(StatusCodes.Status400BadRequest)]
+        [SwaggerResponse(StatusCodes.Status500InternalServerError)]
+        public ActionResult createDanhMuc([FromBody] Hang hang)
+        {
+            try
+            {
+                // kết nối db
+                var connectionString = "Server=LOIDV-IT-PC1\\SQLEXPRESS;Database=QLKHO;Trusted_Connection=true";
+                SqlConnection connection = new SqlConnection(connectionString);
+                connection.Open();
+
+                // Chuẩn bị query 
+                String createQuery = "INSERT INTO HANG(MaHang, DMID, NCCID, TenHang, MauSac, SoLuong, DonGia)" +
+                                        "VALUES(@mahang, @dmid, @nccid, @tenhang, @mausac, @soluong, @dongia);";
+
+                // Chuẩn bị param
+                DynamicParameters parameters = new DynamicParameters();
+                parameters.Add("@mahang", hang.MaHang);
+                parameters.Add("@dmid", hang.DMID);
+                parameters.Add("@nccid", hang.NCCID);
+                parameters.Add("@tenhang", hang.TenHang);
+                parameters.Add("@mausac", hang.MauSac);
+                parameters.Add("@soluong", hang.SoLuong);
+                parameters.Add("@dongia", hang.DonGia);
+
+                //Thực thi query
+                var result = connection.Execute(createQuery, parameters);
+                connection.Close();
+                //Trả về
+                if (result > 0)
+                {
+                    return StatusCode(StatusCodes.Status200OK, hang.MaHang);
+                }
+                return StatusCode(StatusCodes.Status400BadRequest);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest);
+            }
+        }
+
+        [HttpPut("{mahang}")]
+        [SwaggerResponse(StatusCodes.Status200OK, type: typeof(string))]
+        [SwaggerResponse(StatusCodes.Status400BadRequest)]
+        [SwaggerResponse(StatusCodes.Status500InternalServerError)]
+        public ActionResult updateHang([FromRoute] string? mahang, [FromBody] Hang hang)
+        {
+            try
+            {
+                // kết nối db
+                var connectionString = "Server=LOIDV-IT-PC1\\SQLEXPRESS;Database=QLKHO;Trusted_Connection=true";
+                SqlConnection connection = new SqlConnection(connectionString);
+                connection.Open();
+
+                // Chuẩn bị query 
+                String updateQuery = "UPDATE HANG SET DMID = @dmid, NCCID = @nccid, TenHang = @tenhang, MauSac = @mausac, SoLuong = @soluong, DonGia = @dongia WHERE MaHang = @mahang;";
+
+                // Chuẩn bị param
+                DynamicParameters parameters = new DynamicParameters();
+                parameters.Add("@mahang", mahang);
+                parameters.Add("@dmid", hang.DMID);
+                parameters.Add("@nccid", hang.NCCID);
+                parameters.Add("@tenhang", hang.TenHang);
+                parameters.Add("@mausac", hang.MauSac);
+                parameters.Add("@soluong", hang.SoLuong);
+                parameters.Add("@dongia", hang.DonGia);
+
+                //Thực thi query
+                var result = connection.Execute(updateQuery, parameters);
+                connection.Close();
+                //Trả về
+                if (result > 0)
+                {
+                    return StatusCode(StatusCodes.Status200OK, mahang);
+                }
+                return StatusCode(StatusCodes.Status400BadRequest);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest);
+            }
+        }
+    }
+}
---
+++ Nhom2_usol_Api_QLKHO/Nhom2_usol_Api_QLKHO/Controllers/NhanVienController.cs
@@ -0,0 +1,182 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Nhom2_usol_Api_QLKHO.Entities;
+using Swashbuckle.AspNetCore.Annotations;
+using System;
+
+namespace Nhom2_usol_Api_QLKHO.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NhanVienController : ControllerBase
+    {
+        /// <summary>
+        /// Api lấy thông tin tất cả nhân viên
+        /// </summary>
+        /// <returns>Danh sách nhân viên</returns>
+        [HttpGet]
+        [SwaggerResponse(StatusCodes.Status200OK, type: typeof(NhanVien))]
+        [SwaggerResponse(StatusCodes.Status400BadRequest)]
+        [SwaggerResponse(StatusCodes.Status500InternalServerError)]
+        public IActionResult GetAllNhanVien()
+        {
+            try
+            {
+                // kết nối db
+                var connectionString = "Server=LOIDV-IT-PC1\\SQLEXPRESS;Database=QLKHO;Trusted_Connection=true";
+                SqlConnection connection = new SqlConnection(connectionString);
+                connection.Open();
+
+                // Chuẩn bị query 
+                String getQuery = "SELECT * FROM NHANVIEN;";
+
+                //Thực thi query
+                var result = connection.Query<NhanVien>(getQuery);
+                connection.Close();
+                //Trả về
+                if (result == null)
+                {
+                    return StatusCode(StatusCodes.Status400BadRequest);
+                }
+                return StatusCode(StatusCodes.Status200OK, result);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest);
+            }
+        }
+
+        /// <summary>
+        /// Api xóa nhân viên theo mã nhân viên
+        /// </summary>
+        /// <param name="manv"></param>
+        /// <returns>Mã nhân viên của nhân viên bị xóa</returns>
+        [HttpDelete("{manv}")]
+        [SwaggerResponse(StatusCodes.Status200OK, type: typeof(string))]
+        [SwaggerResponse(StatusCodes.Status400BadRequest)]
+        [SwaggerResponse(StatusCodes.Status500InternalServerError)]
+        public ActionResult deleteNhanVienByMaNV([FromRoute] string? manv)
+        {
+            try
+            {
+                // kết nối db
+                var connectionString = "Server=LOIDV-IT-PC1\\SQLEXPRESS;Database=QLKHO;Trusted_Connection=true";
+                SqlConnection connection = new SqlConnection(connectionString);
+                connection.Open();
+
+                // Chuẩn bị query 
+                String deleteQuery = "DELETE FROM NHANVIEN WHERE MaNV = @manv;";
+
+                // Chuẩn bị param
+                DynamicParameters parameters = new DynamicParameters();
+                parameters.Add("@manv", manv);
+
+                //Thực thi query
+                var result = connection.Execute(deleteQuery, parameters);
+                connection.Close();
+                //Trả về
+                if (result > 0)
+                {
+                    return StatusCode(StatusCodes.Status200OK, manv);
+                }
+                return StatusCode(StatusCodes.Status400BadRequest);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest);
+            }
+        }
+
+        /// <summary>
+        /// Api tạo danh mục mới
+        /// </summary>
+        /// <param name="nhanvien"></param>
+        /// <returns>mã danh mục đc tạo</returns>
+        [HttpPost]
+        [SwaggerResponse(StatusCodes.Status200OK, type: typeof(string))]
+        [SwaggerResponse(StatusCodes.Status400BadRequest)]
+        [SwaggerResponse(StatusCodes.Status500InternalServerError)]
+        public ActionResult createNhanVien([FromBody] NhanVien nhanvien)
+        {
+            try
+            {
+                // kết nối db
+                var connectionString = "Server=LOIDV-IT-PC1\\SQLEXPRESS;Database=QLKHO;Trusted_Connection=true";
+                SqlConnection connection = new SqlConnection(connectionString);
+                connection.Open();
+
+                // Chuẩn bị query 
+                String createQuery = "INSERT INTO NHANVIEN(MaNV, TenNV, DiaChi, ChucVu)" +
+                                        "VALUES(@manv, @tennv, @diachi, @chucvu);";
+
+                // Chuẩn bị param
+                DynamicParameters parameters = new DynamicParameters();
+                parameters.Add("@manv", nhanvien.MaNV);
+                parameters.Add("@tennv", nhanvien.TenNV);
+                parameters.Add("@diachi", nhanvien.DiaChi);
+                parameters.Add("@chucvu", nhanvien.ChucVu);
+
+                //Thực thi query
+                var result = connection.Execute(createQuery, parameters);
+                connection.Close();
+                //Trả về
+                if (result > 0)
+                {
+                    return StatusCode(StatusCodes.Status200OK, nhanvien.MaNV);
+                }
+                return StatusCode(StatusCodes.Status400BadRequest);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest);
+            }
+        }
+
+        /// <summary>
+        /// Api cập nhật thông tin nhân viên
+        /// </summary>
+        /// <param name="manv"></param>
+        /// <param name="nhanvien"></param>
+        /// <returns>mã nhân viên của nhân viên được cập nhật</returns>
+        [HttpPut("{manv}")]
+        [SwaggerResponse(StatusCodes.Status200OK, type: typeof(string))]
+        [SwaggerResponse(StatusCodes.Status400BadRequest)]
+        [SwaggerResponse(StatusCodes.Status500InternalServerError)]
+        public ActionResult updateNhanVien([FromRoute] string? manv, [FromBody] NhanVien nhanvien)
+        {
+            try
+            {
+                // kết nối db
+                var connectionString = "Server=LOIDV-IT-PC1\\SQLEXPRESS;Database=QLKHO;Trusted_Connection=true";
+                SqlConnection connection = new SqlConnection(connectionString);
+                connection.Open();
+
+                // Chuẩn bị query 
+                String updateQuery = "UPDATE NHANVIEN SET TenNV = @tennv, DiaChi = @diachi, ChucVu = @chucvu WHERE MaNV = @manv;";
+
+                // Chuẩn bị param
+                DynamicParameters parameters = new DynamicParameters();
+                parameters.Add("@manv", manv);
+                parameters.Add("@tennv", nhanvien.TenNV);
+                parameters.Add("@diachi", nhanvien.DiaChi);
+                parameters.Add("@chucvu", nhanvien.ChucVu);
+
+                //Thực thi query
+                var result = connection.Execute(updateQuery, parameters);
+                connection.Close();
+                //Trả về
+                if (result > 0)
+                {
+                    return StatusCode(StatusCodes.Status200OK, manv);
+                }
+                return StatusCode(StatusCodes.Status400BadRequest);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status400BadRequest);
+            }
+        }
+    }
+}
